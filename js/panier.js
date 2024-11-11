04f01a5e52ac4b9157151e7f4bd24bce6d061415
Coderyounes <younesanas2021@gmail.com>
2024-11-11 20:29:53 +0100

    feat: (SCRUM-73) : build a Dynamic component to increment & decrement quantite

--- js/panier.js
+++ js/panier.js
@@ -1,6 +1,15 @@
 let incr = document.getElementById('incr');
-let decr = document.getElementById('incr');
+let decr = document.getElementById('decr');
+let quantite = document.getElementById('quantite');
+quantite.textContent = 1;
 
+incr.addEventListener('click', () => {
+    quantite.textContent = parseInt(quantite.textContent) + 1;
+});
 
-incr.addEventListener('click', increment);
-decr.addEventListener('click', decrement);
+decr.addEventListener('click', () => {
+    let currentQuantite = parseInt(quantite.textContent);
+    if (currentQuantite > 0) {
+        quantite.textContent = currentQuantite - 1;
+    }
+});
