be99aa0964c06a449623e3df980f4e7e82bd2abc
Malika <elabiadmalika64@gmail.com>
2024-11-12 17:09:03 +0100

    feat: (SCRUM-69) Ajuster le burger menu de page d'accueil  #

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,7 @@ module.exports = {
         'custom-32': '32px',
         'custom-15':'15px',
         'custom-20':'20px',
+        'custom-16':'16px',
       },
       colors: {
         'custom-gray': '#929292',
