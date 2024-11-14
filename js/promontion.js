469ddcbacb9ebe6232b62f32cf369bd0384c4d47
Hamza <hamzacoude@gmail.com>
2024-11-14 18:37:23 +0100

    [SCRUM-71]add  btn 2 slaider

--- js/promontion.js
+++ js/promontion.js
@@ -1,2 +1,4 @@
+// btn slaider 1 to promontion
 let btn_page1=document.querySelector(".page1-promontion");
+// btn slaider 2 to promontion
 let btn_page2=document.querySelector(".page2-promontion");
