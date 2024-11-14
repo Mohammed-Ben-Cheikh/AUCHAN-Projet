bb83e8340704f5ce93db84b24eebbb49b94ec652
Hamza <hamzacoude@gmail.com>
2024-11-14 19:42:07 +0100

    [SCRUM-71]add  action to btn

--- js/promontion.js
+++ js/promontion.js
@@ -1,4 +1,13 @@
 // btn slaider 1 to promontion
-let btn_page1=document.querySelector(".page1-promontion");
+let btn_page1=document.querySelector(".btnPAGE1");
 // btn slaider 2 to promontion
-let btn_page2=document.querySelector(".page2-promontion");
+let btn_page2=document.querySelector(".btnPAGE2");
+
+btn_page2.onclick=function(){
+    document.querySelector('.page1-promontion').classList.add('hidden');
+    document.querySelector('.page2-promontion').classList.remove('hidden');
+}
+btn_page1.onclick=function(){
+    document.querySelector('.page1-promontion').classList.remove('hidden');
+    document.querySelector('.page2-promontion').classList.add('hidden');
+}
