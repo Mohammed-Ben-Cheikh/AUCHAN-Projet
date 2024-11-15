5ddcbfc6fa7a293d3f590e3eb4dd8ef6023aeca8
Malika <elabiadmalika64@gmail.com>
2024-11-15 13:02:15 +0100

    feat: (SCRUM-73) Ajouter function showProduct depuis une api avec xmlHttpRequest()

--- js/categorie.js
+++ js/categorie.js
@@ -5,5 +5,14 @@ connection.send()
 connection.onreadystatechange = function () {
     if (connection.readyState === 4 && connection.status === 200) {
         let data = JSON.parse(connection.responseText)
+        console.log(data);
+        function showProducts(data) {
+
+            document.getElementById("products-container").innerHTML = '';
+            data.forEach(element => {
+                document.getElementById("products-container").innerHTML += `
+                <div class="bg-white w-[18rem] flex flex-col justify-end gap-6 items-center rounded-[1rem] py-3 px-2">
+                
+
     }
 }
