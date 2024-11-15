a0236b8945a3b90c26398eec2e4e13e36766a3ef
Hounaida202 <etrichih@gmail.com>
2024-11-15 16:20:46 +0100

    (SCRUM-69) ajouter le resposive pour la section des categories , pour la version desktop

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
 let container=document.querySelector(".cartes");
-let btn=document.querySelector(".")
+let button=document.querySelector(".btnn");
+let cardes=document.querySelector(".categories-card");
+
+button.addEventListener('click', function() {
+    // Vérifier si la section est cachée
+    if (cardes.style.display === 'none' || cardes.style.display === '') {
+        cardes.style.display = 'block'; // Afficher la section
+    } else {
+        cardes.style.display = 'none'; // Cacher la section
+    }
+});
